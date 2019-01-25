6f3a6f5c3aec2cd49255f088ce02fa9a5baf61de
Alexey Lyutov <lyutov.alexey@gmail.com>
2019-01-25 16:39:42 +0100

    Generalized layout generator
    - Generalized the layout generator procedure. Now it can take an incomplete layout as an input and fill it.
    - Merged the procedure of drawing pizza with the main file.
    - Added proper docstrings to functions

--- layout_generator.py
+++ layout_generator.py
@@ -1,8 +1,18 @@
 import random
 import codecs
+import numpy as np
+import matplotlib.pyplot as plt
+# random.seed(0)
 
 
 def read_setup(fname):
+    """
+    Reading the setup from the input file 'fname'.
+    pizza -- 2D list of pizza contents
+    n_row, n_col -- number of rows and columns in pizza
+    L -- minimum amount of each ingridient in a slice
+    H -- maximum size of a slice
+    """
     with codecs.open(fname, 'r') as fin:
         n_row, n_col, L, H = list(map(int, fin.readline().split()))
 
@@ -15,6 +25,10 @@ def read_setup(fname):
 
 
 def generate_possible_slices(_L, _H):
+    """
+    Generates a list of all possible slices based on _L and _H.
+    Each slice is encoded as (l, h) - it's length and height, respectively.
+    """
     n_min = 2 * _L
     n_max = _H
 
@@ -28,11 +42,54 @@ def generate_possible_slices(_L, _H):
     return slices
 
 
+def draw_pizza(pizza):
+    """
+    Draws the image of pizza into the "pizza_image.pdf" as 2D colormap
+    """
+    n_row = len(pizza)
+    n_col = len(pizza[0])
+
+    mtr = np.zeros((n_row, n_col), dtype=int)
+    for i in range(n_row):
+        row = pizza[i]
+        _row = row.replace('T', '1')
+        _row = _row.replace('M', '0')
+        _row = list(map(int, list(_row)))
+        mtr[i] = np.array(_row)
+
+    scale = 8 / max(n_row, n_col)
+    # print((n_row * scale, n_col * scale))
+
+    plt.figure(figsize=(n_row * scale, n_col * scale))
+    plt.imshow(mtr, cmap='YlOrBr')
+    plt.gca().axes.get_xaxis().set_visible(False)
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    plt.savefig("pizza_image.pdf", bbox_inches = 'tight')
+    # plt.show()
+
+
+def isolated_cell(c_used, x, y, n_row, n_col):
+    """
+    Checks if there is a border or a filled cell to the left or down the (x,y).
+    If so, then the cell is isolated and nothing would fit there.
+    """
+    if ((x+1 >= n_col) or bool(c_used[x+1 + y * n_col])) and ((y+1 >= n_row) or bool(c_used[x + (y+1) * n_col])):
+        return True
+    return False
+
+
 def exceeds_boundary(x, y, l, h, n_row, n_col):
+    """
+    Checks, if the slice (l, h) placed at (x, y) would exceed one of the pizza boundaries
+    """
     return (x+l > n_col) or (y+h > n_row)
 
 
 def collides_w_used(c_used, x, y, l, h):
+    """
+    Checks, if the slice (l, h) placed at (x, y) would overlap with one of the used pizza cells
+    """
     for i in range(y, y+h):
         for j in range(x, x+l):
             c = j + i * n_col
@@ -42,6 +99,9 @@ def collides_w_used(c_used, x, y, l, h):
 
 
 def enough_contents(pizza, _L, x, y, l, h):
+    """
+    Checks, if the slice (l, h) placed at (x, y) would have enough of M and T contents
+    """
     T = 0
     M = 0
     
@@ -57,8 +117,44 @@ def enough_contents(pizza, _L, x, y, l, h):
     return (T >= _L) and (M >= _L)
 
 
-def generate_layout(c_empty, c_used, n_col, n_row):
-    layout = []
+def fill_empty_used(layout, slices, n_col):
+    """
+    The procedure fills lists 'c_empty' and 'c_used' based on the input layout
+    """
+    c_empty = [1 for _ in range(n_row * n_col)]
+    c_used = [0 for _ in range(n_row * n_col)]
+
+    for pos, k in layout:
+        y = pos // n_col
+        x = pos - y * n_col
+        l, h = slices[k]
+        for i in range(y, y+h):
+            for j in range(x, x+l):
+                c = j + i * n_col
+                c_used[c] = 1
+                c_empty[c] = 0
+
+    return c_empty, c_used
+
+
+def generate_layout(layout, slices, n_col, n_row):
+    """
+    Procedure generates a new layout of slices based on the one in input.
+    Input layout might empty or filled to some extent.
+    Input layout is assumed to be correct, such that none of slices do exceed the boundary, overlap with other slices, or do not satisfy the contents condition.
+
+    layout  --  gene of an individual encoded as tuples (pos, k)
+                pos is the position on the pizza, 0 <= pos <= n_col * n_row - 1
+                k is the index of slice that has its upper left cell at this pos
+
+
+    c_empty --  cells that are empty and a slice can use them
+    c_used  --  cells that are occupied by either a slice, or by randomly assigned unused cell
+                it might be necessary in future to have a different list for unused cells
+    """
+
+    c_empty, c_used = fill_empty_used(layout, slices, n_col)
+    
     for pos in range(n_row * n_col):
         if not c_empty[pos]:
             continue
@@ -72,6 +168,10 @@ def generate_layout(c_empty, c_used, n_col, n_row):
         
         y = pos // n_col
         x = pos - y * n_col
+        if isolated_cell(c_used, x, y, n_row, n_col):
+            # cell 'pos' is isolated. Nothing can be fitted here, skipping.
+            # print(" - pos=%d is isolated. Skipping it."%pos)
+            continue
 
         # Trying to place each type of slice in a random order
         i_slice = [i for i in range(len(slices))]
@@ -94,77 +194,27 @@ def generate_layout(c_empty, c_used, n_col, n_row):
 
 
 
-# pizza, n_row, n_col, _L, _H = read_setup("a_example.in")
-pizza, n_row, n_col, _L, _H = read_setup("b_small.in")
-# pizza, n_row, n_col, _L, _H = read_setup("c_medium.in")
-# pizza, n_row, n_col, _L, _H = read_setup("d_big.in")
-
+pizza, n_row, n_col, _L, _H = read_setup("input/c_medium.in") # 
 slices = generate_possible_slices(_L, _H)
 
 # print("Slices: ", slices)
-# for row in pizza:
-#     print(row)
+draw_pizza(pizza)
+
+for i in range(len(slices)):
+    print(i, slices[i])
+
 
 print("Max score is %d"%(n_col * n_row))
-scores = []
-# random.seed(0)
-for i in range(1):
-    if i % 500 == 0:
-        print("  i = %d"%i)
-    """
-    c_empty --  cells that are empty and a slice can use them
-    c_used  --  cells that are occupied by either a slice, or by randomly assigned unused cell
-                it might be necessary in future to have a different list for unused cells
-    layout  --  gene of an individual encoded as tuples (pos, k)
-                pos is the position on the pizza, 0 <= pos <= n_col * n_row - 1
-                k is the index of slice that has its upper left cell at this pos
-    """
-    c_empty = [1 for _ in range(n_row * n_col)]
-    c_used = [0 for _ in range(n_row * n_col)]
+# layout = [(0, 4), (1, 8), (2, 3), (10, 0), (12, 8), (13, 4), (16, 0), (18, 6), (23, 0)]
+layout = []
 
-    c_empty, c_used, layout = generate_layout(c_empty, c_used, n_col, n_row)
+c_empty, c_used, layout = generate_layout(layout, slices, n_col, n_row)
 
 
         
-    efficiency = 100 * (1 - sum(c_empty) / n_row / n_col)
-    scores.append(efficiency)
-    print("The efficiency of the created layout = %5.2f%%; score is %d"%(efficiency, sum(c_used)))
-    print(layout)
+efficiency = 100 * (1 - sum(c_empty) / n_row / n_col)
+print("The efficiency of the created layout = %5.2f%%; score is %d"%(efficiency, sum(c_used)))
+# print(layout)
 
 
-    # if efficiency > 99.0:
-        # print("SUCCESS on iteration %d!"%i)
-        # print(layout)
-        # break
 
-# for i in range(len(slices)):
-#     print(i, slices[i])
-
-
-"""
-def draw_hist():
-    # Draw the goal function histogram
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-
-    def halven_bins(b):
-        res = []
-        for i in range(len(b)-1):
-            res.append(0.5*(b[i]+b[i+1]))
-        return res
-
-    plt.figure(figsize=(5, 5))
-    plt.grid(alpha = 0.5, linestyle = '--', linewidth = 0.2, color = 'black')
-    plt.xlabel('Scores')
-    plt.ylabel('Frequency')
-
-    bbins = np.linspace(60, 101, 15) # np.geomspace(0.0001, 5500, 25)
-
-    c, b = np.histogram(scores, bins=bbins)
-    plt.plot(halven_bins(b), c/100, 'o-', markersize=4.0, markerfacecolor='none', markeredgewidth=1.0)
-
-    # plt.xscale('log')
-    # plt.savefig("results/Hist-gf(d,v)=log_p=%s.png"%i2s(p_ID), dpi=300, bbox_inches = 'tight')
-    plt.show()
-"""
